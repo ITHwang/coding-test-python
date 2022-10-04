6fb1a76b10f92de5ad051a6bfa5873908985ce89
intaek hwang <63974732+ITHwang@users.noreply.github.com>
2022-10-04 10:55:29 +0000

    solved 2 probs

--- programmers/2022-군장병sw교육/Lv3_야근 지수.py
+++ programmers/2022-군장병sw교육/Lv3_야근 지수.py
@@ -1,16 +1,17 @@
-def solution(works, n):
+def solution(n, works):
     if sum(works) <= n: return 0
     mean = sum(works) // len(works)
     works.sort(reverse=True)
     for i in range(len(works)):
         if works[i] > mean and n > 0:
             gap = works[i] - mean
-            works[i] -= gap
-            n -= gap
-            if n < 0:
+            if n < gap:
                 works[i] -= n
                 n = 0
-    if n == 0: return sum([w*w for w in works])
+            else:
+                works[i] -= gap
+                n -= gap
+    if n == 0: return sum([w * w for w in works])
     k = n // len(works)
     n -= k * len(works)
     for j in range(len(works)):
@@ -20,5 +21,4 @@ def solution(works, n):
             if n > 0 and works[j] > 0:
                 works[j] -= 1
                 n -= 1
-    return sum([w*w for w in works])
-# not yet
+    return sum([w * w for w in works])
